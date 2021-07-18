1fb070a76b41fac052ccbcbcf41d97390d6d5e7c
DaniilGorin <DaniilGorin@users.noreply.github.com>
2021-07-18 14:58:46 +1000

    +

--- Assets/Scripts/AI_Mover.cs
+++ Assets/Scripts/AI_Mover.cs
@@ -8,14 +8,20 @@ public class AI_Mover : MonoBehaviour
 {
     [SerializeField] private Transform _fightPlace;
     [SerializeField] private Transform _idlePlace;
+    private Rigidbody rb;
     private float value;
 
     //[SerializeField] private GameObject _worker;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
-        value += 0.001f;
-        GetComponent<Rigidbody>().transform.position = Vector3.Lerp(_fightPlace.position, _idlePlace.position, value);
+        value += Time.deltaTime / 3;
+        rb.transform.position = Vector3.Lerp(_fightPlace.position, _idlePlace.position, value);
 
         if (value > 1) return;
     }
